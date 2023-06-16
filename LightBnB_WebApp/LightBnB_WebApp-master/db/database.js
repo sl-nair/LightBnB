b4b1ad6fddad14c68622d9305564d81cf52caf3d
Sethu Nair <sethunair2102@gmail.com>
2023-06-16 16:11:43 -0600

    add properties insert

--- LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
@@ -157,7 +157,38 @@ const addProperty = function (property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
-  return Promise.resolve(property);
+  // return Promise.resolve(property);
+  const values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    true,
+  ];
+
+  return pool.query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+  VALUES
+  ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;`, values)
+  .then(res => {
+    if(res.rows.length === 0){
+      return null;
+    }
+    return res.rows[0];
+  })
+  .catch(err => console.error('query error', err.stack));
+
+
+
 };
 
 module.exports = {
