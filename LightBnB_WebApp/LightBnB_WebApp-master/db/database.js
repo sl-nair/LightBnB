e0683f4fc5bc79239dc0d376da734bcc1b720439
Sethu Nair <sethunair2102@gmail.com>
2023-06-16 16:31:30 -0600

    img added

--- LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
@@ -16,14 +16,14 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithEmail = function (email) {
+const getUserWithEmail = function(email) {
   return pool.query(`SELECT * FROM users WHERE email = $1`, [email])
-  .then(res => {
-    if(res.rows.length === 0){
-      return null;
-    }
-    return res.rows[0];
-  }).catch(err => console.error('query error', err.stack));
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows[0];
+    }).catch(err => console.error('query error', err.stack));
 };
 
 /**
@@ -31,15 +31,14 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function (id) {
+const getUserWithId = function(id) {
   return pool.query(`SELECT * FROM users WHERE id = $1`, [id])
-  .then(res => {
-    if(res.rows.length === 0){
-      return null;
-    }
-    return res.rows[0];
-  }).catch(err => console.error('query error', err.stack));
-
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows[0];
+    }).catch(err => console.error('query error', err.stack));
 };
 
 /**
@@ -47,24 +46,22 @@ const getUserWithId = function (id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
+const addUser = function(user) {
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
-  // return Promise.resolve(user);
 
   return pool.query(`INSERT INTO users (
     name, email, password) 
     VALUES (
     $1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
-  .then(res => {
-    if(res.rows.length === 0){
-      return null;
-    }
-    return res.rows[0];
-  })
-  .catch(err => console.error('query error', err.stack));
-
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows[0];
+    })
+    .catch(err => console.error('query error', err.stack));
 };
 
 /// Reservations
@@ -74,7 +71,7 @@ const addUser = function (user) {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
+const getAllReservations = function(guest_id, limit = 10) {
   return pool.query(`SELECT properties.* 
   FROM properties 
   JOIN reservations ON properties.id = property_id 
@@ -83,14 +80,13 @@ const getAllReservations = function (guest_id, limit = 10) {
   GROUP BY reservations.id , properties.id
   ORDER BY reservations.start_date
   LIMIT $2;`, [guest_id, limit])
-  .then(res => {
-    if(res.rows.length === 0){
-      return null;
-    }
-    return res.rows;
-  })
-  .catch(err => console.error('query error', err.stack));
-  
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows;
+    })
+    .catch(err => console.error('query error', err.stack));
 };
 
 /// Properties
@@ -101,29 +97,27 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  
+const getAllProperties = function(options, limit = 10) {
   const queryParams = [];
-  
   let queryString = `Select properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id`;
-// filtering through city
+
   if (options.city) {
     queryParams.push(`${options.city}`);
-    queryString += ` WHERE city LIKE $${queryParams.length}`
-  }
-  //filtering through owner
-  if (options.owner_id) {
-    queryParams.push(`${options.owner_id}`);
-    queryString += ` WHERE owner_id = $${queryParams.length}`
+    queryString += ` WHERE city LIKE $${queryParams.length}`;
   }
 
-  if (options.minimum_price_per_night && options.maximum_price_per_night){
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += ` WHERE owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(`${options.minimum_price_per_night * 100.0}`);
     queryParams.push(`${options.maximum_price_per_night * 100.0}`);
     queryString += ` WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}`;
-  } else if (options.maximum_price_per_night){
+  } else if (options.maximum_price_per_night) {
     queryParams.push(`${options.maximum_price_per_night * 100.0}`);
     queryString += ` WHERE cost_per_night <= $${queryParams.length}`;
   } else if (options.minimum_price_per_night) {
@@ -131,19 +125,17 @@ const getAllProperties = function (options, limit = 10) {
     queryString += ` WHERE cost_per_night >= $${queryParams.length}`;
   }
 
-  if (options.minimum_rating ) {
+  if (options.minimum_rating) {
     queryParams.push(`${options.minimum_rating}`);
     queryString += ` GROUP BY properties.id HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
   } else {
     queryString += ` GROUP BY properties.id`;
   }
+
   queryParams.push(limit);
-  queryString +=`
+  queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};`;
-
-  console.log(queryString, queryParams);
-
   return pool.query(queryString, queryParams)
     .then((res) => res.rows);
 };
@@ -153,7 +145,7 @@ const getAllProperties = function (options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
+const addProperty = function(property) {
   const propertyId = Object.keys(properties).length + 1;
   property.id = propertyId;
   properties[propertyId] = property;
@@ -179,16 +171,13 @@ const addProperty = function (property) {
   return pool.query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
   VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) RETURNING *;`, values)
-  .then(res => {
-    if(res.rows.length === 0){
-      return null;
-    }
-    return res.rows[0];
-  })
-  .catch(err => console.error('query error', err.stack));
-
-
-
+    .then(res => {
+      if (res.rows.length === 0) {
+        return null;
+      }
+      return res.rows[0];
+    })
+    .catch(err => console.error('query error', err.stack));
 };
 
 module.exports = {
---
+++ README.md
@@ -0,0 +1,25 @@
+# LightBnB
+
+## Project Summary
+Lighthouse BnB is an app that will revolutionize the travel industry. It will allow homeowners to rent out their homes to people on vacation, creating an alternative to hotels and bed and breakfasts...There’s nothing else like it! Users can view property information, book reservations, view their reservations, and write reviews. We'll be creating the first ever application to do something like this and we will call it LighthouseBnB.
+
+## Setup
+``` npm install ```
+
+## Server startup
+``` cd LightBnB_WebApp/LightBnB_WebApp-master/ ```
+
+``` npm run local ```
+
+## Running database
+``` startpostgres```
+``` psql -h localhost -p 5432 -U labber lightbnb ```
+The password is 123.
+
+### Dependencies
+    "bcrypt": "^3.0.6",
+    "cookie-session": "^1.3.3",
+    "express": "^4.17.1",
+    "nodemon": "^1.19.1",
+    "pg": "^8.11.0"
+
